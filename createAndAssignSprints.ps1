f1e5db83efc470492f82198169e06a91b2a7e11a
JREneria <jayareneria1218@live.com>
2026-04-08 19:51:36 +0800

    Update createAndAssignSprints.ps1

--- createAndAssignSprints.ps1
+++ createAndAssignSprints.ps1
@@ -115,7 +115,7 @@ else {
     $projectInfo = Invoke-AdoRest -Method GET -Uri $projectInfoUri
     $projectId = $projectInfo.id
 
-    Write-Host ${Organization}
+    Write-Host $projectInfoUri
 
     if (-not $projectId) {
         throw "Failed to resolve project ID for project '$Project'"
