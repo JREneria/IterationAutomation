152b69729f7fb548e9416c599e708133e92ca6fe
JREneria <jayareneria1218@live.com>
2026-04-17 14:47:37 +0800

    Update createTeam.ps1

--- createTeam.ps1
+++ createTeam.ps1
@@ -157,7 +157,7 @@ function Create-AreaNode {
         $parentPath = "/" + (($ParentSegments | ForEach-Object { [uri]::EscapeDataString($_) }) -join "/")
     }
 
-    $uri = "$Org/$ProjectEsc/_apis/wit/classificationnodes/Areas${parentPath}?api-version=${ApiVersionWit}"   # Classification Nodes CreateOrUpdate tps://medium.com/@kanerika/power-automate-vs-logic-apps-2025-full-comparison-of-microsoft-automation-tools-f569b42f2cea)[7](https://www.linkedin.com/pulse/power-automate-vs-logic-apps-finding-right-microsoft-tool-nishad-owgne)
+    $uri = "$Org/$ProjectEsc/_apis/wit/classificationnodes/Areas${parentPath}?api-version=${ApiVersionWit}"   
     Write-Host $uri
     if ($DryRun -or -not $PSCmdlet.ShouldProcess(($fullPath -join '\'), "Create Area Node")) {
         Write-Host "[Area] DryRun/WhatIf: would create $($fullPath -join '\')"
@@ -171,7 +171,7 @@ function Create-AreaNode {
 function Update-TeamFieldValues {
     param([string]$Org, [string]$ProjectEsc, [string]$TeamEsc, [string]$ProjectName, [string]$TeamName, [string[]]$Clients, [bool]$DryRun)
 
-    $uri = "$Org/$ProjectEsc/$TeamEsc/_apis/work/teamsettings/teamfieldvalues?api-version=$ApiVersionWork"   # Teamfieldvalues Update [8](https://seekatar.github.io/2025/05/05/pipeline-artifacts.html)[9](https://stackoverflow.com/questions/79124230/how-to-publish-and-download-artifacts-as-zip-in-azure-yaml-pipeline)
+    $uri = "$Org/$ProjectEsc/$TeamEsc/_apis/work/teamsettings/teamfieldvalues?api-version=$ApiVersionWork"   
     $teamRoot = "$ProjectName\$TeamName"
 
     $values = @(@{ value = $teamRoot; includeChildren = $true })
@@ -191,7 +191,7 @@ function Update-TeamFieldValues {
 function Get-ProjectScopeDescriptor {
     param([string]$OrgName, [string]$ProjectId)
 
-    $uri = "https://vssps.dev.azure.com/$OrgName/_apis/graph/descriptors/$ProjectId?api-version=7.1"  # Descriptors Get [10](https://stackoverflow.com/questions/78708204/i-want-to-publish-a-zip-file-and-then-download-it-directly-from-azure-artifact-f)
+    $uri = "https://vssps.dev.azure.com/$OrgName/_apis/graph/descriptors/${ProjectId}?api-version=7.1"  # Descriptors Get [10](https://stackoverflow.com/questions/78708204/i-want-to-publish-a-zip-file-and-then-download-it-directly-from-azure-artifact-f)
     (Invoke-AdoRest -Method GET -Uri $uri).value
 }
 
@@ -202,7 +202,7 @@ function Get-GraphGroupsInScope {
     $continuation = $null
     do {
         $ctPart = if ($continuation) { "&continuationToken=$([uri]::EscapeDataString($continuation))" } else { "" }
-        $uri = "https://vssps.dev.azure.com/$OrgName/_apis/graph/groups?scopeDescriptor=$([uri]::EscapeDataString($ScopeDescriptor))$ctPart&api-version=$ApiVersionGraphPreview"  # Groups List /stackoverflow.com/questions/79901604/how-to-get-a-list-of-all-azure-devops-project-users-using-rest-api-with-typescri)[12](https://learn.microsoft.com/en-us/rest/api/azure/devops/graph/descriptors?view=azure-devops-rest-7.1)
+        $uri = "https://vssps.dev.azure.com/$OrgName/_apis/graph/groups?scopeDescriptor=$([uri]::EscapeDataString($ScopeDescriptor))${ctPart}&api-version=${ApiVersionGraphPreview}"  # Groups List /stackoverflow.com/questions/79901604/how-to-get-a-list-of-all-azure-devops-project-users-using-rest-api-with-typescri)[12](https://learn.microsoft.com/en-us/rest/api/azure/devops/graph/descriptors?view=azure-devops-rest-7.1)
 
         $resp = Invoke-WebRequest -Method GET -Uri $uri -Headers $headers
         $json = $resp.Content | ConvertFrom-Json
